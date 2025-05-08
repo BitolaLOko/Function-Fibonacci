ca8645be14fe595fa8692744e49345f8f95da21e
Victor-Furtado <victorfurtadosantos@gmail.com>
2025-05-08 15:09:14 -0300

    Função loop, função recursiva que executa a sequencia Fibonacci

---
+++ fibonacci.js
@@ -0,0 +1,28 @@
+let argumento = 0;
+
+for (let i = 2; i < process.argv.length; i++) {
+  argumento += Number(process.argv[i]);
+}
+
+function loop(n) {
+  let fib = [0, 1];
+  for (let i = 2; i <= n; i++) {
+    fib[i] = fib[i - 1] + fib[i - 2];
+    console.log(fib[i - 2]);
+  }
+}
+
+loop(argumento + 2);
+
+function recursiva(n) {
+  if (n == 0) {
+    return 0;
+  }
+  if (n == 1) {
+    return 1;
+  }
+
+  return recursiva(n - 1) + recursiva(n - 2);
+}
+
+console.log(recursiva(argumento));
